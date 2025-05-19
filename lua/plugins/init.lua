e60d9c97368c5fe0e5744abd0faedd68e4b38592
monoguitari <jesus_rodriguez@brown.edu>
2025-05-18 21:00:03 -0400

    efreaa olften

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,13 +16,49 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "javascript",
+        "typescript", "tsx", "css",
+  		},
+  	},
+  },
+  --
+  -- below this is stuff I added
+  -- following https://www.youtube.com/watch?v=8um8OYwvz3c
+  --
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettierd",
+        "eslint-lsp",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      }
+    }
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+       return require "configs.null-ls"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "html",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end
+  }
 }
